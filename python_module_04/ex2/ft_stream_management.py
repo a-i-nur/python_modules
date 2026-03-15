45f372540a79172035f4d415a135fefdd13ea560
a-i-nur <broklya@gmail.com>
2026-03-15 17:48:42 +0100

    Fix output

--- python_module_04/ex2/ft_stream_management.py
+++ python_module_04/ex2/ft_stream_management.py
@@ -5,15 +5,27 @@ import sys
 
 def ft_stream_management() -> None:
     print("=== CYBER ARCHIVES - COMMUNICATION SYSTEM ===")
+    print()
+
     archivist_id = input("Input Stream active. Enter archivist ID: ")
     status_report = input("Input Stream active. Enter status report: ")
-    print(f"[STANDARD] Archive status from {archivist_id}: {status_report}")
+    # alternatively, we could read from stdin directly:
+    # status_report = sys.stdin.readline()
+    print()
+
+    print(
+        f"[STANDARD] Archive status from "
+        f"{archivist_id}: {status_report}", file=sys.stdout)
     print(
         "[ALERT] System diagnostic: Communication channels verified",
-        file=sys.stderr,
-    )
-    print("[STANDARD] Data transmission complete")
-    print("Three-channel communication test successful.")
+        file=sys.stderr)
+    # alternatively, we could write to stderr directly:
+    # sys.stderr.write(
+    #    "[ALERT] System diagnostic: Communication channels verified\n")
+    print("[STANDARD] Data transmission complete", file=sys.stdout)
+    print()
+
+    sys.stdout.write("Three-channel communication test successful.\n")
 
 
 if __name__ == "__main__":
